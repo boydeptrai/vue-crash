fa0d28d1f32849e1bb7404f9f99a96a00a3346e1
Tuan Tran <langthamht047@gmail.com>
2022-11-17 08:40:28 -0800

    initial

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>Task Trackers</h1>
+  </header>
+</template>
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Header",
+};
+</script>
+<style scoped>
+ header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+ }
+</style>
