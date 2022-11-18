f2e3d08b9faed95325b7d7ac74874234069cb12a
Tuan Tran <langthamht047@gmail.com>
2022-11-17 20:42:44 -0800

    the task component

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,12 +1,19 @@
 <template>
-    <div v-for="task in tasks" :key="task.id"><h3>{{task.text}}</h3></div>
+    <div v-for="task in tasks" :key="task.id">
+
+    <TheTask :task="task" />
+    </div>
 </template>
 <script>
+import TheTask from './TheTask.vue'
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Tasks',
     props: {
         tasks: Array
+    },
+    components: {
+        TheTask
     }
 }
 </script>
